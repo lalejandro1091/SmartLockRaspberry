db17f387b7a44fb80cb777c4f26c7ba5152e8f70
lalejandro1091 <lalejandro1091@hotmail.com>
2016-05-15 02:09:57 +0000

    primer commit de raspberry

---
+++ cerebro.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python2.7
+import serial
+import urllib2
+import time
+
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+while True:
+  req = urllib2.Request('http://sys.bugs3.com/server/sys/controller/TOTP.php')
+  response = urllib2.urlopen(req)
+  code  = str(response.read())
+  code = code[0:6]
+  arduino.write(code)
+  print (code)
+  time.sleep(20)
