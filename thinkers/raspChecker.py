2826953920c63a972ba9a51c84d517563ae79d80
lalejandro1091 <lalejandro1091@hotmail.com>
2016-06-02 20:21:48 +0000

    correccion con carpetas

---
+++ thinkers/raspChecker.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python2.7
+import serial
+import urllib2
+import time
+
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+pascode = "luce1a"
+pascode1 = "luce2a"
+while True:
+  requ = urllib2.Request('http://localhost/php/lucesChecker.php')
+  response1 = urllib2.urlopen(requ).read()
+  code1  = response1.find("luce1e")
+  print response1
+  print code1
+  if pascode == code1:
+    arduino.write("luce1e")
+    print 'se prende'
+  else:
+    arduino.write("luce1a")
+    print 'se apaga'
+  code = response1.find("luce2e")
+  if pascode1 == code1:
+    arduino.write("luce2e")
+    print 'se prende'
+  else:
+    arduino.write("luce2a")
+    print 'se apaga'
+  time.sleep(2)
